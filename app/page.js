9b9c18f9e2fa3bd20cef1b6ae2460564b0e41435
Umamahesh <umamahesh.b@techub.in>
2024-12-01 10:09:51 +0530

    changed the path of image

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { GithubIcon, LinkedinIcon, Globe, Mail, Phone, Download, ArrowRight } from "lucide-react";
 import Image from "next/image";
-import profileImage from "../public/Assets/profileImage.jpeg"
+import profileImage from "../public/Assets/ProfileImage.jpeg"
 import { projects } from "../data/data.js";
 import ProjectsSection from "@/components/projects";
 import ExperienceSection from "@/components/experience";
